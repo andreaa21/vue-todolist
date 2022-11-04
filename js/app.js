6e19d93715abd8efd38a7073519b93df2bfc07b9
andreaa21 <andrea.zollini@proton.me>
2022-11-04 15:25:02 +0100

    add steps 1.2.3.4

---
+++ js/app.js
@@ -0,0 +1,52 @@
+/*
+
+**Descrizione:**
+Rifare l’esercizio della to do list.
+Ogni todo sarà un oggetto, formato da due proprietà:
+- `text`, una stringa che indica il testo del todo
+- `done`, un booleano (true/false) che indica se il todo è stato fatto oppure no
+Steps:
+1. avere un elenco di task da fare o fatti
+2. stamparli in pagina
+3. al click del task fare il toggle del task fatto o meno
+4. al click della croce eliminare il task se fatto, altrimenti scrivere un messaggio di errore
+5. dopo aver scritto il task con invio o al click del bottone aggiungere il nuovo task (come non fatto) in cima all’elenco dei task
+6. verificare la lunghezza minima del nuovo task
+7. se l’elenco è vuoto non mostrare la lista ma mostrare il messaggio “Non ci sono task”
+
+*/
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            logoImg: 'img/logo.png',
+            errorMsg: '',
+            tasks:[
+                {
+                    text: 'Sviluppare un carousel con autoscroll',
+                    done: false
+                },
+                {
+                    text: 'Portare a spasso il cane',
+                    done: false
+                },
+                {
+                    text: 'Altra cosa da fare che non mi viene in mente',
+                    done: false
+                }
+            ]
+        }
+    },
+    methods: {
+        deleteTask(index){
+            this.errorMsg = '';
+            if(this.tasks[index].done){
+                this.tasks.splice(index,1)
+            }else{
+                this.errorMsg = "Puoi eliminare un task solo se è stato fatto!"
+            }
+        }
+    }
+}).mount('#app')
